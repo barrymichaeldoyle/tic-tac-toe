fc44ec0ea05d2311e161786d282e0466bc423a04
Barry Michael Doyle <barry@barrymichaeldoyle.com>
2020-05-06 11:04:55 +0200

    simplified board jsx

--- src/pages/room/index.tsx
+++ src/pages/room/index.tsx
@@ -61,19 +61,19 @@ const Room = () => {
     <Container>
       <h3>{message}</h3>
       <Row>
-        <Block onClick={() => handleClick(0)}>{board[0] && board[0]}</Block>
-        <Block onClick={() => handleClick(1)}>{board[1] && board[1]}</Block>
-        <Block onClick={() => handleClick(2)}>{board[2] && board[2]}</Block>
+        <Block onClick={() => handleClick(0)}>{board[0]}</Block>
+        <Block onClick={() => handleClick(1)}>{board[1]}</Block>
+        <Block onClick={() => handleClick(2)}>{board[2]}</Block>
       </Row>
       <Row>
-        <Block onClick={() => handleClick(3)}>{board[3] && board[3]}</Block>
-        <Block onClick={() => handleClick(4)}>{board[4] && board[4]}</Block>
-        <Block onClick={() => handleClick(5)}>{board[5] && board[5]}</Block>
+        <Block onClick={() => handleClick(3)}>{board[3]}</Block>
+        <Block onClick={() => handleClick(4)}>{board[4]}</Block>
+        <Block onClick={() => handleClick(5)}>{board[5]}</Block>
       </Row>
       <Row>
-        <Block onClick={() => handleClick(6)}>{board[6] && board[6]}</Block>
-        <Block onClick={() => handleClick(7)}>{board[7] && board[7]}</Block>
-        <Block onClick={() => handleClick(8)}>{board[8] && board[8]}</Block>
+        <Block onClick={() => handleClick(6)}>{board[6]}</Block>
+        <Block onClick={() => handleClick(7)}>{board[7]}</Block>
+        <Block onClick={() => handleClick(8)}>{board[8]}</Block>
       </Row>
       <Button onClick={handleClear}>Clear Board</Button>
     </Container>
