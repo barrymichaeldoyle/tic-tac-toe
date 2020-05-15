3a90c8f6999b6d2f16293b82fb95400cecd6aa70
Barry Michael Doyle <barry@barrymichaeldoyle.com>
2020-05-15 17:26:02 +0200

    updated teachings

--- src/pages/teachings/index.tsx
+++ src/pages/teachings/index.tsx
@@ -1,20 +1,37 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 
+import { H1 } from 'components'
 import { db } from 'services'
+import { User } from 'typings'
 
 const Teachings: FC = () => {
-  const userCollection = db
-    .collection('users')
-    .get()
-    .then(function (snapshot) {
-      snapshot.forEach((doc) =>
-        console.log(doc.id, '=>', doc.data().displayName)
+  const [isFetching, setIsFetching] = useState(true)
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    const unsubscribe = db.collection('users').onSnapshot((snapshot) => {
+      const updateUsers = snapshot.docs.map(
+        (doc) => ({ ...doc.data(), id: doc.id } as User)
       )
+      setUsers(updateUsers)
+      setIsFetching(false)
     })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [])
+
+  if (isFetching) return <H1>Fetching Users...</H1>
 
   return (
     <>
       <h1>teachings</h1>
+      {users.map((user) => (
+        <div>
+          {user.id} - {user.displayName} - {user.roomId}
+        </div>
+      ))}
     </>
   )
 }
